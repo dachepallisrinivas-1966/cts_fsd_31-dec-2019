f27cf362f5b6b5855bfbefb029cc111f168af9a0
Dachepalli Srinvas <dachepallisrinivas@gmail.com>
2020-01-16 21:21:14 +0530

    callable statement programs added

--- JDBCDemo/src/com/cts/jdbcd/ui/CallableInParameterDemo.java
+++ JDBCDemo/src/com/cts/jdbcd/ui/CallableInParameterDemo.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Scanner;
 
-public class CallableDemo {
+public class CallableInParameterDemo {
 
 	public static void main(String[] args) {
 		try (
